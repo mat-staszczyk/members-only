8cf422c82d5897efe5b5a4ad0a4dac5f4f86c7d0
sleaz0id <mat.staszczyk@gmail.com>
2016-12-29 20:49:26 +0100

    Static pages controller and minitest reporters

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get root_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+  end
+
+end
